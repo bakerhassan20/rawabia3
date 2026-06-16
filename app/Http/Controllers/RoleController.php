192802c064acd7b83c4d2ec13bbb76cfa181d660
abobaker hassan <abobakerhassan60@gmail.com>
2026-06-17 01:15:21 +0300

    update

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
